7b0bf6168df3c00359adb46e319ce8644f8098cc
lalinnx <76035493+lalinnx@users.noreply.github.com>
2020-12-15 16:44:25 +0700

    Update lab5_1.cpp

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -11,7 +11,7 @@ int main(){
         cout << "Enter an integer: ";
         cin >> n;
         if(n != 0){
-			if(n%2 == 0){
+		if(n%2 == 0){
             	Even++;
         	}else odd++;
         }
